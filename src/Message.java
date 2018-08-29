20381eb54fe1f48e37d1bc0e888bc006c6a44295
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 18:21:04 -0500

    Initial commit

---
+++ src/Message.java
@@ -0,0 +1,9 @@
+
+public class Message {
+	public static void main (String[] args) {
+		System.out.println("Kevin Tran");
+		System.out.println("I learned how to create a simple GUI with a custom title bar using the JFrame class");
+	}
+
+}
+
